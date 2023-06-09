89b21c41a507a9fce6da54ceb3337a49f5b2bb94
tiwavaldese <tiwaval7@gmail.com>
2023-06-09 14:55:24 +0100

    script.js

--- script.js
+++ script.js
@@ -1,100 +1,90 @@
 
-let copyText = document.querySelector(".copy-text");
-copyText.querySelector("button").addEventListener("click", function(){
-    let input= copyText.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText.classList.remove("active");
-}, 2500 )
-})
+let  copyText = document.getElementById('copy-text').innerHTML;
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
+  let  copyText1 = document.getElementById('copy').innerHTML;
+  const copyContent1 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText1);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
-let copyText1 = document.querySelector(".copy");
-copyText1.querySelector("button").addEventListener("click", function(){
-    let input= copyText1.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText1.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText1.classList.remove("active");
-}, 2500 )
-})
+  let  copyText2 = document.getElementById('copied').innerHTML;
+  const copyContent2 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText2);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
-let copyText2 = document.querySelector(".copied");
-copyText2.querySelector("button").addEventListener("click", function(){
-    let input= copyText2.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText2.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText2.classList.remove("active");
-}, 2500 )
-})
+  let  copyText3 = document.getElementById('copying').innerHTML;
+  const copyContent3 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText3);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
+  let  copyText4 = document.getElementById('copies').innerHTML;
+  const copyContent4 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText4);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
-let copyText3= document.querySelector(".copying");
-copyText3.querySelector("button").addEventListener("click", function(){
-    let input= copyText3.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText3.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText3.classList.remove("active");
-}, 2500 )
-})
+  let  copyText5 = document.getElementById('copie').innerHTML;
+  const copyContent5 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText5);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
+  let  copyText6 = document.getElementById('copy-txt').innerHTML;
+  const copyContent6 = async () => {
+    try {
+      await navigator.clipboard.writeText(copyText6);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
-let copyText4= document.querySelector(".copies");
-copyText4.querySelector("button").addEventListener("click", function(){
-    let input= copyText4.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText4.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText4.classList.remove("active");
-}, 2500 )
-})
+  let  copyText7 = document.getElementById('copied-txt').innerHTML;
+  const copyContent7= async () => {
+    try {
+      await navigator.clipboard.writeText(copyText7);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  
+  }
 
-let copyText5= document.querySelector(".copie");
-copyText5.querySelector("button").addEventListener("click", function(){
-    let input= copyText5.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText5.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText5.classList.remove("active");
-}, 2500 )
-})
-
-
-let copyText6= document.querySelector(".copy-txt");
-copyText6.querySelector("button").addEventListener("click", function(){
-    let input= copyText6.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText6.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText6.classList.remove("active");
-}, 2500 )
-})
-
-let copyText7= document.querySelector(".copied-txt");
-copyText7.querySelector("button").addEventListener("click", function(){
-    let input= copyText7.querySelector(".text");
-   input.focus();
-   document.execCommand("copy");
-copyText7.classList.add("active");
-window.getSelection().removeAllRanges();
-setTimeout(function(){
-    copyText7.classList.remove("active");
-}, 2500 )
-})
+  
